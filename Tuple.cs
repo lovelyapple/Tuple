8dedf3306b4c52f5b1e4ba748fea0426725c8322
linli <lovelyappleoffice@gmail.com>
2017-05-16 12:31:35 +0900

    file

---
+++ Tuple.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Tuple<T1,T2>
+{
+    #region Variables
+    public T1 item1;
+    public T2 item2;
+
+    private static readonly IEqualityComparer Item1Cpmparer = EqualityComparer<T1>.Default;
+    private static readonly IEqualityComparer<T2> Item2Comparer = EqualityComparer<T2>.Default;
+
+    #endregion
+
+
+    #region Constructors
+
+    public Tuple(T1 _item1,T2 _item2)//originally is _internal_
+    {
+        item1 = _item1;
+        item2 = _item2;
+    }
+
+    #endregion
+
+    #region Public Frunctions
+    public override string ToString ()
+    {
+        return string.Format ("<{0},`1,}>", item1, item2);
+    }
+
+    #endregion
+
+    #region Private Functions
+
+    private static bool IsNull(object obj)
+    {
+        return object.ReferenceEquals(obj, null);
+    }
+
+    #endregion
+
+    #region Opreators
+
+    public static bool operator ==(Tuple<T1, T2> a, Tuple<T1, T2> b)
+    {
+        if (Tuple<T1, T2>.IsNull(a) && !Tuple<T1, T2>.IsNull(b))
+            return false;
+
+        if (!Tuple<T1, T2>.IsNull(a) && Tuple<T1, T2>.IsNull(b))
+            return false;
+
+        if (Tuple<T1, T2>.IsNull(a) && Tuple<T1, T2>.IsNull(b))
+            return true;
+        
+        return
+            a.item1.Equals(b.item1) &&
+            a.item2.Equals(b.item2);
+    }
+
+    public static bool operator != (Tuple<T1, T2> a, Tuple<T1, T2> b)
+    {
+        return !(a == b);
+    }
+
+    #endregion
+}
+
+
---
+++ testMain.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class testMain : MonoBehaviour {
+    //public Tuple<int,int> tuple = new Tuple<int, int>();
+    public Dictionary<Tuple<int,int>,int> dic = new Dictionary<Tuple<int,int>, int>();
+    public List<Tuple<int,int>> list = new List<Tuple<int,int>>();
+
+    public int stats = 0;
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        switch (stats) {
+        case 0:
+            break;
+        case 1:
+            Tuple<int,int> tupleTemp1 = new Tuple<int, int> (1, 1);
+            Tuple<int,int> tupleTemp2 = new Tuple<int, int> (2, 2);
+            Tuple<int,int> tupleTemp3 = new Tuple<int, int> (3, 3);
+            dic.Add (tupleTemp1,1);
+            list.Add (tupleTemp1);
+            dic.Add (tupleTemp2,2);
+            list.Add (tupleTemp2);
+            dic.Add (tupleTemp3,3);
+            list.Add (tupleTemp3);
+            stats = 0;
+            break;
+        case 2:
+            foreach (Tuple<int,int> element in dic.Keys) {
+                Debug.Log ("dic" + element.item1 + element.item2);
+            }
+            foreach (Tuple<int,int> element in list) {
+                Debug.Log ("dic" + element.item1 + element.item2);
+            }
+            stats = 0;
+            break;
+        case 3:
+            Tuple<int,int> target = new Tuple<int, int> (1, 1);
+
+            int res;
+
+            if (dic.TryGetValue (target,out res))
+                Debug.Log ("found in dic");
+            //if (list.Find (new Tuple<int,int>(1,1)))
+            //    Debug.Log ("found in list");
+            stats = 0;
+            break;
+
+        }
+
+    }
+}
